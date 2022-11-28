376946347eea0d7761f45812fb43a1a53d9e3b9a
2022-DEV1-101 <bouraoui.ziche@gmail.com>
2022-11-28 19:35:11 +0000

    creation of the api to create new player

--- src/main/java/com/game/tictactoe/controller/TicTacToeController.java
+++ src/main/java/com/game/tictactoe/controller/TicTacToeController.java
@@ -1,5 +1,31 @@
 package com.game.tictactoe.controller;
 
-public class TicTacToeController {
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.game.tictactoe.entity.Player;
+import com.game.tictactoe.requests.NewPlayerRequest;
+import com.game.tictactoe.services.PlayerService;
+import com.game.tictactoe.view.PlayerView;
+
+@RestController
+@RequestMapping(path = "/api")
+public class TicTacToeController {
+	private final PlayerService playerService;
+
+	public TicTacToeController(PlayerService playerSerice) {
+		this.playerService = playerSerice;
+	}
+
+	@PostMapping(value = "/player/new")
+	@ResponseBody
+	public ResponseEntity<PlayerView> newPlayer(@RequestBody NewPlayerRequest playerReq) {
+		Player player = new Player(playerReq.getUserName());
+		return new ResponseEntity<>(this.playerService.save(player), HttpStatus.CREATED);
+	}
 }
--- src/test/java/com/game/tictactoe/controller/TicTacToeControllerTest.java
+++ src/test/java/com/game/tictactoe/controller/TicTacToeControllerTest.java
@@ -7,12 +7,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.game.tictactoe.requests.NewPlayerRequest;
+import com.game.tictactoe.services.PlayerService;
 
 @WebMvcTest(TicTacToeController.class)
 public class TicTacToeControllerTest {
@@ -21,7 +23,8 @@ public class TicTacToeControllerTest {
 	@Autowired
 	private ObjectMapper objectMapper;
 
-	
+	@MockBean
+	private PlayerService playerService;
 
 	@Test
 	@DisplayName("new Player api test")
@@ -29,7 +32,7 @@ public class TicTacToeControllerTest {
 		NewPlayerRequest req = new NewPlayerRequest(); 
 		req.setUserName("player");
 
-		mockMvc.perform(post("/player/new")
+		mockMvc.perform(post("/api/player/new")
 				.contentType(MediaType.APPLICATION_JSON).content(objectMapper.writeValueAsString(req)))
 				.andExpect(status().isCreated());
 	}
